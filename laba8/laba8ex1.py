25c83a114ffaa2e0093224241a7599ca26201370
Надежда Ворокова <135130452+NadyaVorokovaRUDN@users.noreply.github.com>
2023-06-07 17:36:41 +0300

    laba8ex1.py

--- laba8/laba8ex1.py
+++ laba8/laba8ex1.py
@@ -1,22 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.loadtxt(fname = 'grafik.txt', delimiter = ' ')
+a = np.loadtxt(fname = 'grafik.txt', delimiter = ' ')
 
 Y = []
 X1 = []
 X2 = []
 
-for i in range(len(data)):
+for i in range(len(a)):
     for j in range(3):
         if j == 0:
-            Y.append(data[i][j])
+            Y.append(a[i][j])
         elif j == 1:
-            X1.append(data[i][j])
+            X1.append(a[i][j])
         else:
-            X2.append(data[i][j])
+            X2.append(a[i][j])
             
-plt.title('График двух зависимостей')
 plt.xlabel('rA')
 plt.ylabel('V')
 plt.plot(Y, X1, label = "Va")
